8b19bec4aa76b0ec8b6c5310ceed9a1a8eae4e8f
tuligma <ricardopulupotjr@gmail.com>
2025-03-07 03:58:04 +0400

    done!

--- D00/ex00/megaphone.cpp
+++ D00/ex00/megaphone.cpp
@@ -6,28 +6,52 @@
 /*   By: npentini <npentini@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 23:59:28 by npentini          #+#    #+#             */
-/*   Updated: 2024/05/17 02:26:05 by npentini         ###   ########.fr       */
+/*   Updated: 2025/03/07 03:56:03 by npentini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
+#include <string>
+
+class Louder
+{
+	private:
+	char loud;
+	std::string word;
+
+	public:
+	
+	int saySomething(int argc)
+	{
+		if (argc < 2)
+		{
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+			return (1);
+		}
+		return (0);
+	}
+	
+	void sayItLoud(int argc, char *argv[])
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			word = argv[i];
+			for (int j = 0; j < word.length(); j++)
+			{
+				loud = toupper(word[j]);
+				std::cout << loud;
+			}
+		}
+	};
+
+};
 
 int	main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (1);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] >= 97 && argv[i][j] >= 92)
-				argv[i][j] -= 32;
-			std::cout << argv[i][j];
-		}
-	}
+	Louder speak;
+	if (speak.saySomething(argc) == 0)
+		speak.sayItLoud(argc, argv);
 	std::cout << '\n';
 	return (0);
 }
